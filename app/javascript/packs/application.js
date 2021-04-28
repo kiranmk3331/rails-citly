b283fcd3be6f7c431845f4eea7b3336306753fdb
kiranmk3331 <kiranmk3331@gmail.com>
2021-04-28 11:11:36 +0530

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
 import "../stylesheets/application.scss";
-import "channels"
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
