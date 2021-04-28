117025b3f1c97dcb636e38dc2d7a3398b35053a1
kiranmk3331 <kiranmk3331@gmail.com>
2021-04-28 12:02:24 +0530

    Added API based architecture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,14 @@
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
-import "../stylesheets/application.scss";
 import "channels";
+import "../stylesheets/application.scss";
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+// Support component names relative to this directory:
+
 var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from "react";
+import { initializeLogger } from "common/logger";
+import { logger } from "common/logger";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Logger from "js-logger";
 
 const App = () => {
   useEffect(() => {
     /*eslint no-undef: "off"*/
     initializeLogger();
-    logger.info("Log from js-logger");
+    Logger.info("Log from js-logger");
   }, []);
 
   return (
     <Router>
       <Switch>
         <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/about" render={() => <div>About new things</div>} />
       </Switch>
     </Router>
   );
